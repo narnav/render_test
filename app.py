a283d21b0e7c1df401147d2c23cc057dbbff05c2
eyal <narnavg@gmail.com>
2022-11-09 18:46:20 +0200

    sec

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
- 
+
+ar=[{"name":"betty","age":20}]
+
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return f'Hello, World!{ar[0]}'
  
  
 if __name__ == '__main__':
